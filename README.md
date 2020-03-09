879c7ca8e02a5357856c63cdbc79da8e524eba7a
cristinaponay <cristinaponay@gmail.com>
2020-03-08 22:54:24 -0300

    show all halifax buses

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # halifax-transit-tracker
 Know when the next bus is coming!
 
-Limitation: buses 1-10 only
-
 See it in action: http://cristinaponay.com/halifax-transit-tracker/
--- main.js
+++ main.js
@@ -2,11 +2,24 @@
 
     let DATA_URL = "https://hrmbuses.herokuapp.com/";
 
+/***  little hack starts here ***/
+    // source: http://jsfiddle.net/paulovieira/yVLJf/
+    L.Map = L.Map.extend({
+        openPopup: function (popup) {
+            //        this.closePopup();  // just comment this
+            this._popup = popup;
+
+            return this.addLayer(popup).fire('popupopen', {
+                popup: this._popup
+            });
+        }
+    }); /***  end of hack ***/
+
     //create map in leaflet and tie it to the div called 'theMap'
     var map = L.map('theMap').setView([44.650627, -63.597140], 14);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Data by <a href="https://www.halifax.ca/home/open-data/halifax-transit-open-data">Halifax Transit</a>,'
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Data by <a href="https://www.halifax.ca/home/open-data/halifax-transit-open-data">Halifax Transit</a>'
     }).addTo(map);
 
     var busIcon = L.icon({
@@ -27,7 +40,7 @@
 
                 // console.log(myJson.entity);
                 let geojsonFeatureBus = myJson.entity
-                    .filter((bus) => parseInt(bus.vehicle.trip.routeId) <= 10)
+                    // .filter((bus) => parseInt(bus.vehicle.trip.routeId) <= 10)
                     .map((bus) => {
                         return {
                             type: "Feature",
@@ -58,17 +71,27 @@
                 //  plot the markers on the map
                 markers = L.geoJSON(geojsonFeatureBus, {
                     pointToLayer: function (feature, latlng) {
-                        var marker = L.marker(latlng, { icon: busIcon, rotationAngle: feature.properties.bearing });
+                        let start_date;
+                        if (feature.properties.start_date.length !== 8) {
+                            start_date = feature.properties.start_date;
+                        }
+                        else {
+                            let year = feature.properties.start_date.slice(0, 4);
+                            let month = feature.properties.start_date.slice(4, 6);
+                            let day = feature.properties.start_date.slice(6, 8);
+                            start_date = new Date(month + " " + day + " " + year).toDateString();
+                            console.log(feature.properties.start_date);
+                        }
+
+                        let marker = L.marker(latlng, { icon: busIcon, rotationAngle: feature.properties.bearing });
                         marker.bindPopup(
+                            '<h3>Bus #' + feature.properties.route_id + '</h3>' +
                             'Unique ID: ' + feature.properties.unique_id + '<br/>' +
                             'Trip ID: ' + feature.properties.trip_id + '<br/>' +
-                            'Start Date: ' + feature.properties.start_date + '<br/>' +
-                            'Route ID: ' + feature.properties.route_id + '<br/>' +
+                            'Start Date: ' + start_date + '<br/>' +
                             'Speed: ' + feature.properties.speed + '<br/>' +
-                            'Bearing: ' + feature.properties.bearing + '<br/>' +
-                            'Vehicle ID: ' + feature.properties.veh_id + '<br/>' +
-                            'Label: ' + feature.properties.label + '<br/>'
-                        );
+                            'Vehicle ID: ' + feature.properties.veh_id + '<br/>'
+                        ).openPopup();
                         return marker;
                     }
                 }).addTo(map);
